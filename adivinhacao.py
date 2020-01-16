b8cb19093ecb74be369bdd39041a4bdf6dd15a63
GustavoDell <gustavo.dell@outlook.com>
2020-01-16 00:06:27 -0300

    Organizando ainda melhor nosso codigo

--- adivinhacao.py
+++ adivinhacao.py
@@ -60,3 +60,5 @@ def jogar():
 
     print("Fim do jogo")
 
+if(__name__ == "__main__"):
+    jogar()
--- jogos.py
+++ jogos.py
@@ -1,17 +1,20 @@
 import forca
 import adivinhacao
 
-print("*********************************")
-print("*******Escolha o seu jogo!*******")
-print("*********************************")
+def escolha_jogo():
+    print("*********************************")
+    print("*******Escolha o seu jogo!*******")
+    print("*********************************")
 
-print("(1) Forca (2) Adivinhação")
+    print("(1) Forca (2) Adivinhação")
 
-jogo = int(input("Qual jogo?"))
+    jogo = int(input("Qual jogo?"))
 
-if(jogo == 1):
-    print("Jogando Forca")
-    forca.jogar()
-elif(jogo == 2):
-    print("Jogando adivinhação")
-    adivinhacao.jogar()
+    if(jogo == 1):
+        print("Jogando Forca")
+        forca.jogar()
+    elif(jogo == 2):
+        print("Jogando adivinhação")
+        adivinhacao.jogar()
+
+if (__name__ == "__main__"):
